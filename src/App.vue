7ff3fe1f6f3cef2ae167cb8e6933e0367c6b5574
takasmile <takasmile@outlook.com>
2019-02-10 14:22:07 +0800

    express test success

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <span class="title is-size-1">NoteTaker</span>
+          </a>
+        </div>
+      </div>
+    </nav>
+    <br><br>
+    <div  class="container">
+      <div v-if="notes">
+        <CreateNoteModal @createNote="createNote"/>
+        <br>
+        <div>
+          <noteItem v-for="(note, index) in notes" :note="note" :key="index" @deleteNote="deleteNote" @updateNote="updateNote"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import NoteItem  from './components/NoteItem.vue'
+  import CreateNoteModal  from './components/CreateNoteModal.vue'
+  import { getNotes }  from './repository'
+  export default {
+    name: 'app',
+    components: { NoteItem, CreateNoteModal  },
+    data(){
+      return {
+        notes: []
+      }
+    },
+    methods: {
+      deleteNote(id){
+        let notes = this.notes.filter(note => note._id != id)
+        this.notes = notes;
+      },
+      updateNote(note){
+        this.deleteNote(note._id);
+        this.createNote(note);
+      },
+      createNote(note){
+        this.notes = [note, ...this.notes];
+      },
+    },
+    mounted(){
+      getNotes()
+              .then(data => this.notes = data.notes)
+              .catch((err =>alert(err)));
+    }
   }
-}
 </script>
 
 <style>
